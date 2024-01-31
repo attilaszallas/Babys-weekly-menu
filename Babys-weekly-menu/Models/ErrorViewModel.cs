1fb09db7f25da94d097970a184ae3843f8ec6af2
Attila Szallas <ASzallas@slb.com>
2024-01-31 22:25:22 +0100

    Initial working application

---
+++ Babys-weekly-menu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Babys_weekly_menu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
