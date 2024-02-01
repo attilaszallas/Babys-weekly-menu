fbcbde743d2c511db36f2b288a573409478f7667
Attila Szallas <ASzallas@slb.com>
2024-02-01 10:57:02 +0100

    Add Allergy and Ingredient Controllers

---
+++ Babys-weekly-menu/Controllers/AllergyController.cs
@@ -0,0 +1,74 @@
+﻿using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    public class AllergyController : Controller
+    {
+        private readonly WeeklyMenuDbContext _dBcontext;
+
+        public AllergyController(WeeklyMenuDbContext dbContext)
+        {
+            _dBcontext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var allergies = _dBcontext.Allergies.ToList();
+
+            return View(allergies);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Allergy allergy)
+        {
+            _dBcontext.Allergies.Add(allergy);
+            _dBcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var allergy = _dBcontext.Allergies.Find(id);
+
+            return View(allergy);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Allergy allergy)
+        {
+            _dBcontext.Allergies.Update(allergy);
+
+            _dBcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var allergy = _dBcontext.Allergies.Find(id);
+
+            return View(allergy);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Allergy allergy)
+        {
+            _dBcontext.Remove(allergy);
+            _dBcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Babys-weekly-menu/Controllers/IngredientController.cs
@@ -0,0 +1,74 @@
+﻿using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private readonly WeeklyMenuDbContext _dBcontext;
+
+        public IngredientController(WeeklyMenuDbContext dbContext)
+        {
+            _dBcontext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var ingredients = _dBcontext.Ingredients.ToList();
+
+            return View(ingredients);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Ingredient ingredient)
+        {
+            _dBcontext.Ingredients.Add(ingredient);
+            _dBcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var ingredient = _dBcontext.Ingredients.Find(id);
+
+            return View(ingredient);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Ingredient ingredient)
+        {
+            _dBcontext.Ingredients.Update(ingredient);
+
+            _dBcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var ingredient = _dBcontext.Ingredients.Find(id);
+
+            return View(ingredient);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Ingredient ingredient)
+        {
+            _dBcontext.Remove(ingredient);
+            _dBcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
