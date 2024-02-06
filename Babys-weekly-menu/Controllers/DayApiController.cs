e0d09d165f36678d62e3596eb680f6de6bfb1cc9
Attila Szallas <ASzallas@slb.com>
2024-02-06 12:42:43 +0100

    Add Recipe and Day Api Controller

---
+++ Babys-weekly-menu/Controllers/DayApiController.cs
@@ -0,0 +1,100 @@
+﻿using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DayApiController : ControllerBase
+    {
+        private readonly WeeklyMenuDbContext _context;
+
+        public DayApiController(WeeklyMenuDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException();
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        [Route("/api/days")]
+        public async Task<ActionResult> GetAllDays()
+        {
+            var days = await _context.Days.ToListAsync();
+            if (days == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(days);
+        }
+
+        [HttpGet]
+        [Route("/api/day/{id}")]
+        public async Task<ActionResult> GetDay(int id)
+        {
+            var selectedDay = await _context.Days.FindAsync(id);
+            if (selectedDay == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(selectedDay);
+        }
+
+        [HttpPost]
+        [Route("/api/day/{id}")]
+        public async Task<ActionResult<Day>> PostDay(Day day)
+        {
+            _context.Days.Add(day);
+            await _context.SaveChangesAsync();
+
+            return new JsonResult(day);
+        }
+
+        [HttpPut]
+        [Route("/api/day/{id}")]
+        public async Task<ActionResult> PutDay(int id, Day day)
+        {
+            if (id != day.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(day).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Days.Any(d => d.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("/api/day/{id}")]
+        public async Task<ActionResult<Day>> DeleteDay(int id)
+        {
+            var day = await _context.Days.FindAsync(id);
+
+            if (day == null) { return NotFound(); }
+
+            _context.Days.Remove(day);
+            await _context.SaveChangesAsync();
+
+            return new JsonResult(day);
+        }
+    }
+}
---
+++ Babys-weekly-menu/Controllers/RecipeApiController.cs
@@ -0,0 +1,100 @@
+﻿using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeApiController : ControllerBase
+    {
+        private readonly WeeklyMenuDbContext _context;
+
+        public RecipeApiController(WeeklyMenuDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException();
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        [Route("/api/recipes")]
+        public async Task<ActionResult> GetAllRecipes()
+        {
+            var recipes = await _context.Recipes.ToListAsync();
+            if (recipes == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(recipes);
+        }
+
+        [HttpGet]
+        [Route("/api/recipe/{id}")]
+        public async Task<ActionResult> GetRecipe(int id)
+        {
+            var selectedRecipe = await _context.Recipes.FindAsync(id);
+            if (selectedRecipe == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(selectedRecipe);
+        }
+
+        [HttpPost]
+        [Route("/api/recipe/{id}")]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return new JsonResult(recipe);
+        }
+
+        [HttpPut]
+        [Route("/api/recipe/{id}")]
+        public async Task<ActionResult> PutRecipe(int id, Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Recipes.Any(r => r.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("/api/recipe/{id}")]
+        public async Task<ActionResult<Recipe>> DeleteRecipe(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe == null) { return NotFound(); }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return new JsonResult(recipe);
+        }
+    }
+}
