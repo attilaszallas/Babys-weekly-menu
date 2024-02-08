7f78099bbbbca09689f592db23434d012423167e
Attila Szallas <ASzallas@slb.com>
2024-02-08 11:32:14 +0100

    Regenerate Allergy and Ingredient Controllers and Views

---
+++ Babys-weekly-menu/Controllers/AllergiesController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    public class AllergiesController : Controller
+    {
+        private readonly WeeklyMenuDbContext _context;
+
+        public AllergiesController(WeeklyMenuDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Allergies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Allergies.ToListAsync());
+        }
+
+        // GET: Allergies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var allergy = await _context.Allergies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (allergy == null)
+            {
+                return NotFound();
+            }
+
+            return View(allergy);
+        }
+
+        // GET: Allergies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Allergies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Icon")] Allergy allergy)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(allergy);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(allergy);
+        }
+
+        // GET: Allergies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var allergy = await _context.Allergies.FindAsync(id);
+            if (allergy == null)
+            {
+                return NotFound();
+            }
+            return View(allergy);
+        }
+
+        // POST: Allergies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Icon")] Allergy allergy)
+        {
+            if (id != allergy.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(allergy);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AllergyExists(allergy.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(allergy);
+        }
+
+        // GET: Allergies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var allergy = await _context.Allergies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (allergy == null)
+            {
+                return NotFound();
+            }
+
+            return View(allergy);
+        }
+
+        // POST: Allergies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var allergy = await _context.Allergies.FindAsync(id);
+            if (allergy != null)
+            {
+                _context.Allergies.Remove(allergy);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AllergyExists(int id)
+        {
+            return _context.Allergies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Babys-weekly-menu/Controllers/IngredientsController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private readonly WeeklyMenuDbContext _context;
+
+        public IngredientsController(WeeklyMenuDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ingredients
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ingredients.ToListAsync());
+        }
+
+        // GET: Ingredients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ingredients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Icon")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ingredient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Icon")] Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ingredient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IngredientExists(ingredient.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient != null)
+            {
+                _context.Ingredients.Remove(ingredient);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredients.Any(e => e.Id == id);
+        }
+    }
+}
