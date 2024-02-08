3ea55313dbd3189a50ce1bf2d0a8eb554a9327c8
Attila Szallas <ASzallas@slb.com>
2024-02-08 12:36:52 +0100

    Regenerate Meals Controllers and Views

---
+++ Babys-weekly-menu/Controllers/MealsController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BabysWeeklyMenu.Data;
+using BabysWeeklyMenu.Models;
+
+namespace BabysWeeklyMenu.Controllers
+{
+    public class MealsController : Controller
+    {
+        private readonly WeeklyMenuDbContext _context;
+
+        public MealsController(WeeklyMenuDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Meals
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Meals.ToListAsync());
+        }
+
+        // GET: Meals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            return View(meal);
+        }
+
+        // GET: Meals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Meals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Time")] Meal meal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(meal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meal);
+        }
+
+        // GET: Meals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals.FindAsync(id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+            return View(meal);
+        }
+
+        // POST: Meals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Time")] Meal meal)
+        {
+            if (id != meal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MealExists(meal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meal);
+        }
+
+        // GET: Meals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            return View(meal);
+        }
+
+        // POST: Meals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var meal = await _context.Meals.FindAsync(id);
+            if (meal != null)
+            {
+                _context.Meals.Remove(meal);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MealExists(int id)
+        {
+            return _context.Meals.Any(e => e.Id == id);
+        }
+    }
+}
